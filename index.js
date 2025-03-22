d881f79aba3df8dc1bf5f6223a352a310ec8614d
leparan <leparanfelix16@gmail.com>
2025-03-22 11:17:55 +0300

    documentloaded

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+// Set up an event listener for DOMContentLoaded
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  
+    // Target the paragraph element with id="text"
+    const textElement = document.getElementById("text");
+  
+    // Replace the text content with "This is really cool!"
+    textElement.textContent = "This is really cool!";
+  });
+  
+  // This console.log() fires when index.js loads - before DOMContentLoaded is triggered
+  console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
